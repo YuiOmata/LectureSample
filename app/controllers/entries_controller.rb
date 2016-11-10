9e8ed652baf74779025d4a7aee685ff036e22cde
YuiOmata <yuuri.o.drm@gmail.com>
2016-11-10 19:50:13 +0900

    ログインフォームを作成

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
   def sign_in
-    @private_name = cookies[:name]
+    @privious_name = cookies[:name]
     @name = params[:visitor_name]
     cookies[:name] = @name
   end
